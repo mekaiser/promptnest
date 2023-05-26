5f809815ffc3548e48ba0c3a7bddcdab2bde33e5
S M Kaiser Ahmed <kaiser331042@gmail.com>
2023-05-26 17:14:37 +0600

    semi fixes

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -22,7 +22,7 @@ const Nav = () => {
   }, []);
 
   return (
-    <nav className="flex-between w-full mb-16 pt-3">
+    <nav className="flex-between w-full mb-16 pt-4">
       <Link href="/" className="flex gap-2 flex-center">
         <Image
           src="/assets/images/logo.svg"
